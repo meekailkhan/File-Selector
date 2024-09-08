a67d428917f8c7a0f8327ddd8fc4aca1fe806d39
pc <meekailkhan123aar@gmail.com>
2024-09-08 18:01:05 +0530

    file selector project

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
-// const fileBtn = document.getElementById("btn");
-// const fileInput = document.getElementById("fileInput");
-// const imgContainer = document.getElementById("container");
-
-// function eventFn(e){
-//     if(e.type === "click"){
-//         fileInput.click()
-//     }else if(e.type === "change"){
-//         let selectedFile = e.target.files[0];
-//         let img = document.createElement("img");
-//         img.src += selectedFile.name;
-//         imgContainer.append(img)
-//     }
-    
-// }
-
-// fileBtn.addEventListener("click",eventFn)
-// fileInput.addEventListener("change",eventFn)
-
 const fileBtn = document.getElementById("btn");
 const fileInput = document.getElementById("fileInput");
 const imgContainer = document.getElementById("container");
